c1d3b8796932878a9c2b0cd937c967207c9dcd10
Mauricio Rodriguez <maurijrp@gmail.com>
2020-08-09 15:31:38 -0500

    Updates

---
+++ precache-manifest.1036cd096342fad529ea14efec747c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafc16cb33150dcfb8d91803b2aefe40",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "173e5376ad1ab0aed944",
+    "url": "/testReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/testReact/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testReact/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173e5376ad1ab0aed944",
+    "url": "/testReact/static/js/main.1450be1d.chunk.js"
+  },
+  {
+    "revision": "a625425265588f6fa25c",
+    "url": "/testReact/static/js/runtime-main.e3cd38d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
