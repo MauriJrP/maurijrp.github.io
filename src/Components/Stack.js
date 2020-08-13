247c86e64e1588148e9ee4943c15142f4f68b5db
Mauricio Rodriguez <maurijrp@gmail.com>
2020-08-13 15:01:33 -0500

    more technologies added, responsive improved, title atributte added to technologies images

--- src/Components/Stack.js
+++ src/Components/Stack.js
@@ -7,6 +7,8 @@ import css from '../images/css.png';
 import js from '../images/js.png';
 import git from '../images/git.png';
 import github from '../images/github.png';
+import mysql from '../images/mysql.png';
+import react from '../images/react.png';
 
 // -------- ------- ------ -----  ----- ------ ------- --------
 class Stack extends React.Component {
@@ -15,11 +17,33 @@ class Stack extends React.Component {
       <div className="stack">
         <h2>Tecnologías</h2>
         <div className="stack__technologies">
-          <img src={hmtl} alt="" className="stack__photo" />
-          <img src={css} alt="" className="stack__photo" />
-          <img src={js} alt="" className="stack__photo" />
-          <img src={git} alt="" className="stack__photo" />
-          <img src={github} alt="" className="stack__photo" />
+          <img src={hmtl} alt="html" className="stack__photo" title="html" />
+          <img src={css} alt="css" className="stack__photo" ttitle="css" />
+          <img
+            src={js}
+            alt="javaScript"
+            className="stack__photo"
+            title="javaScript"
+          />
+          <img src={git} alt="git" className="stack__photo" ttitle="git" />
+          <img
+            src={github}
+            alt="github"
+            className="stack__photo"
+            title="github"
+          />
+          <img
+            src={mysql}
+            alt="mysql"
+            className="stack__photo"
+            ttitle="mySql"
+          />
+          <img
+            src={react}
+            alt="react"
+            className="stack__photo"
+            ttitle="react"
+          />
         </div>
       </div>
     );
