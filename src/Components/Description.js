c116a2b3259c62e49e91924cc8da738dcda40625
Mauricio Rodriguez <maurijrp@gmail.com>
2020-09-01 22:40:23 -0500

    Description complete, styles of contact section improved

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -8,9 +8,9 @@ class Description extends React.Component {
       <div className="description">
         <h2>¿Quién Soy?</h2>
         <p className="description__text">
-          Tercer semestre de Ingenieria en Computacion, desarrollador frontend.
-          Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur
+          Hola! 🙌 soy Mauricio Rodriguez y actualmente estoy cursando tercer
+          semestre de ingenieria en computacion💻. <br />
+          👨‍💻Desarrollador web originario de Mexico
         </p>
       </div>
     );
